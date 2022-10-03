b6669aa4a54d04c92261d60046c3759c28fa54e1
tons0trees <forrest@fmcclure.com>
2022-10-03 12:01:45 -0600

    as far as we got

--- script.js
+++ script.js
@@ -12,25 +12,47 @@ function makeRow(paramNum) {
 }
 
 const rowButton = document.getElementById('add-row')
-rowButton.addEventListener('click', function(clickEvent) {
+rowButton.addEventListener('click', function() {
     makeRow(10);
 });
-
-function colorize (clickEvent) {
-    if (clickEvent.target.matches('td')) {
-        console.log(clickEvent.target.classList)
-        if (!clickEvent.target.classList.length) {
-            console.log(select)
-            clickEvent.target.classList.toggle(myColor)
-        }
-    } 
-
-}
-
-myTable.addEventListener('click', colorize)
 
 const select = document.getElementsByTagName('select')[0]
 select.addEventListener('change', function(event) {
     console.log(event.target.value)
     myColor = event.target.value
+})
+
+const clearButton = document.getElementById("clear");
+clearButton.addEventListener('click', clearCanvas)
+function clearCanvas() {
+    const tableArray = myTable.children;
+    for (let i=0; i<tableArray.length; i++) {
+        const row = tableArray[i].children
+        for (let j=0; j<row.length; j++) {
+            row[j].classList = [];
+        }
+    }
+}
+
+function colorize(clickEvent) {
+    if (clickEvent.target.matches('td')) {
+        // console.log(clickEvent.target.classList)
+        if (clickEvent.target.classList.length) {
+            // console.log(clickEvent.target.classList)
+            clickEvent.target.classList = []
+        } else {
+            clickEvent.target.classList.add(myColor)
+        }
+    } 
+}
+
+myTable.addEventListener('click', colorize)
+
+
+document.addEventListener('mousedown', function(event) {
+    myTable.addEventListener('mouseover', colorize);
+})
+
+document.addEventListener('mouseup', function(event) {
+    myTable.removeEventListener('mouseover', colorize);
 })
