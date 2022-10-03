c9827dc3fb9737e8a39800103b523bb5e65adadd
tons0trees <forrest@fmcclure.com>
2022-10-03 09:16:35 -0600

    Add row function implemented

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-// Your code here
+const myTable = document.getElementById("mainTable")
+
+function makeRow(paramNum) {
+    const newRow = document.createElement('tr');
+    myTable.appendChild(newRow);
+
+    for (let i=0; i<paramNum; i++) {
+        const newCell = document.createElement('td');
+        newRow.appendChild(newCell);
+    }
+}
+
+const rowButton = document.getElementById('add-row')
+rowButton.addEventListener('click', function(clickEvent) {
+    makeRow(10);
+});
+
+myTable.addEventListener('click', function(clickEvent) {
+    if (clickEvent.target.matches('td')) {
+        clickEvent.target.classList.toggle('red')
+    }
+})
